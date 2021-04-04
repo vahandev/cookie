bd47b570bcd87c50ddbdc543f60686f1dba4dd3b
Vahan Petrosyan <vahan@searchenginejournal.com>
2021-04-04 07:06:58 +0400

    cookie set script

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+header('X-Robots-Tag: noindex');
+// for CORS
+$allowed_origins = array(
+    'http://yourdomain.com'
+);
+
+if( isset( $_SERVER['HTTP_ORIGIN'] ) && in_array( $_SERVER['HTTP_ORIGIN'], $allowed_origins ) ){
+    header("Access-Control-Allow-Origin: ". $_SERVER['HTTP_ORIGIN'] );
+}else{
+    exit;
+}
+
+
+
+header('Access-Control-Request-Method: GET, POST');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
+
+if( isset( $_POST['data'] )){
+    $data = json_decode($_POST['data'], true);
+    setcookie( $data['name'], $data['value'],  (time()+60*60*24*365*2),  $data['path'],  $data['domain'], 0 );
+}
